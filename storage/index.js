045509797bdfa36d2c9cab0029f0757f4f2ec6b6
Ryan Bruno <ryanbruno506@gmail.com>
2018-06-19 19:10:54 -0400

    Started storage

--- storage/index.js
+++ storage/index.js
@@ -1,10 +1,24 @@
 /*
  * /var/mail
  *       /<user>
- *          /<user>-0001.email
- *          /<user>-0002.email
+ *          /Message-ID.email
  */
+const fs = require('fs');
 
 module.exports.store = (mail, config) => {
     console.log('Stored: ' + JSON.stringify(mail));
+    for (mail.to.forEach(address => {
+        const file = '/var/mail/' +  + '/' + mail.messageID + '.email';
+        if (fs.existsSync(file)) {
+            fs.writeFile(file, JSON.stringify(mail), (err) => {
+                if (err) {
+                    console.err(Date.now() + ' ' + err);
+                }
+                console.log(Date.now() + ' Stored (' + mail.messageID + ') in file ' + file);
+            };
+        } else {
+            console.err(Date.now() + ' Mail (' + mail.messageID + ') already exists');
+        }
+    };
+
 };
